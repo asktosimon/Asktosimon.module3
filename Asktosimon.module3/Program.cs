aead2019d62f2ef7a617ff807e0e6f6b58c02b37
Andrey Simanovich <asktosimon@yandex.ru>
2022-01-10 15:38:42 +0300

    Задание 4.1.17

--- Asktosimon.module3/Program.cs
+++ Asktosimon.module3/Program.cs
@@ -4,16 +4,32 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
+		Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-		string MyName = "Jane";
-		byte MyAge = 27;
-		bool HaveIApet = true;
-		double MyShoeSize = 37.5;
+		var color = Console.ReadLine();
 
-		Console.WriteLine("My name is " + MyName);
-		Console.WriteLine("MyAge " + MyAge);
-		Console.WriteLine("Do I have a pet? " + HaveIApet);
-		Console.WriteLine("My shoe size is " + MyShoeSize);
+		switch (color)
+		{
+			case "red":
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
 
+				Console.WriteLine("Your color is red!");
+				break;
+
+			case "green":
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+				break;
+
+			default:
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is cyan!");
+				break;
+		}
 	}
 }
